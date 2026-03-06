9644d02edec7fe0378dfc6cef3c299dfa3e677d5
Rahul kumar <113619190+karnrk@users.noreply.github.com>
2026-03-06 10:48:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import About from './assets/components/pages/About';
 import Education from './assets/components/pages/Education';
 import Layout from './assets/components/Layout';
@@ -10,26 +10,23 @@ import Contact from './assets/components/pages/Contact';
 function App() {
 
   return (
-    <>
-
-      <div id="react-portfolio" className="layout">
-        <div className="layout-content py-4 px-4">
-          <div className="layout-navigation-wrapper flex ">
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<Layout />}>
-                  <Route index element={<About />} />
-                  <Route path="education"  element={<Education />} />
-                  <Route path='experience' element={<Experience />} />
-                  <Route path='portfolio' element={<Portfolio />} />
-                  <Route path='contact' element={<Contact />} />
-                </Route>
-              </Routes>
-            </BrowserRouter>
-          </div>
+    <div id="react-portfolio" className="layout">
+      <div className="layout-content py-4 px-4">
+        <div className="layout-navigation-wrapper flex">
+          <HashRouter>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<About />} />
+                <Route path="education" element={<Education />} />
+                <Route path="experience" element={<Experience />} />
+                <Route path="portfolio" element={<Portfolio />} />
+                <Route path="contact" element={<Contact />} />
+              </Route>
+            </Routes>
+          </HashRouter>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
